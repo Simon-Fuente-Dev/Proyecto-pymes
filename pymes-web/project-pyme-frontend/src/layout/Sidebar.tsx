86974972a1e8577577d6b054ed852390f2ade6b1
Simon-Fuente-Dev <simon.delafuente20@gmail.com>
2025-08-20 22:24:24 -0400

    Cambios sidebar

--- pymes-web/project-pyme-frontend/src/layout/Sidebar.tsx
+++ pymes-web/project-pyme-frontend/src/layout/Sidebar.tsx
@@ -1,50 +1,102 @@
-import { AppBar, Box, CssBaseline, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText, Toolbar } from "@mui/material";
+//Componentes de mui
+import {
+    AppBar,
+    Box,
+    CssBaseline,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Toolbar,
+    Typography,
+    Divider
+} from "@mui/material";
+//Iconos de mui
 import MenuIcon from '@mui/icons-material/Menu';
-import EmailIcon from '@mui/icons-material/Email';
-
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import HomeIcon from '@mui/icons-material/Home';
 import BarChartIcon from '@mui/icons-material/BarChart';
-const drawerWidth = 240;
+import StarIcon from '@mui/icons-material/Star';
+
+import { styled } from '@mui/material/styles';
+
+import { useLocation, useNavigate } from 'react-router-dom'
+
+
+const StyledListItem = styled(ListItem)(({ theme }) => ({
+    cursor: 'pointer',
+    '& .MuiListItemText-primary': {
+        fontSize: '1rem',
+        fontWeight: 500,
+    },
+}))
+
+// const drawerWidth = 220;
+
+const items = [
+    { label: 'Pagina Principal', icon: <HomeIcon />, path: '/sda' },
+    { label: 'Productos y servicios', icon: <BusinessCenterIcon />, path: '/sda' },
+    { label: 'Estadisticas', icon: <BarChartIcon />, path: '/sda' },
+    { label: 'Comentarios y Calificaciones', icon: <StarIcon />, path: '/sda' },
+];
+
 
 const Sidebar = () => {
-
+    const { pathname } = useLocation()
     return (
         <Box sx={{ display: "flex" }}>
             <CssBaseline />
+
+
             {/* Sidebar permanente */}
             <Drawer
                 variant="permanent"
                 sx={{
-                    width: drawerWidth,
                     flexShrink: 0,
-                    [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+                    [`& .MuiDrawer-paper`]: { boxSizing: 'border-box' },
                 }}
             >
                 <Toolbar>
-                    <MenuIcon />
+                    <Typography variant="h6">Mi empresa</Typography>
                 </Toolbar>
+                <Divider />
                 <Box sx={{ overflow: 'auto' }}>
                     <List>
-                        <ListItem button>
+                        {items.map((item) => {
+                            const selected = pathname === item.path;
+                            return (
+                                <StyledListItem button
+                                    key={item.path}
+                                    selected={selected}
+                                    onClick={() => { console.log(item.path) }}
+                                >
+                                    <ListItemIcon>
+                                        {item.icon}
+                                    </ListItemIcon>
+                                    <ListItemText primary={item.label}/>
+                                </StyledListItem>
+                            )
+                        })}
+                        {/* <StyledListItem button>
                             <ListItemIcon>
                                 <HomeIcon />
                             </ListItemIcon>
-                            <ListItemText>Pagina Principal</ListItemText>
-                        </ListItem>
-                        <ListItem button>
+                            <ListItemText primary="Pagina Principal" />
+                        </StyledListItem>
+                        <StyledListItem button>
+                            <ListItemIcon>
+                                <BusinessCenterIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Productos y servicios" />
+                        </StyledListItem>
+                        <StyledListItem button>
                             <ListItemIcon>
                                 <BarChartIcon />
                             </ListItemIcon>
-                            <ListItemText>Estadisticas</ListItemText>
-                        </ListItem>
-                        {/*{['Inbox', 'Starred', 'Send email'].map((text, index) => (*/}
-                        {/*    <ListItem button key={text}>*/}
-                        {/*        <ListItemIcon>*/}
-                        {/*            {index % 2 === 0 ? <MenuIcon /> : <EmailIcon />}*/}
-                        {/*        </ListItemIcon>*/}
-                        {/*        <ListItemText primary={text} />*/}
-                        {/*    </ListItem>*/}
-                        {/*))}*/}
+                            <ListItemText primary="Estadisticas" />
+                        </StyledListItem> */}
                     </List>
                 </Box>
             </Drawer>
