92c2c13825e7df6ad7ddddc2ea05bcba59a32919
Simon-Fuente-Dev <simon.delafuente20@gmail.com>
2025-09-04 17:39:42 -0400

    componnete estrellas

--- pymes-web/project-pyme-frontend/src/components/MainPage/LastCommentCard.tsx
+++ pymes-web/project-pyme-frontend/src/components/MainPage/LastCommentCard.tsx
@@ -1,73 +1,72 @@
-import StarIcon from '@mui/icons-material/Star';
-import StarHalfIcon from '@mui/icons-material/StarHalf';
-import StarOutlineIcon from '@mui/icons-material/StarOutline';
 import {Avatar, Box, Paper, Typography} from "@mui/material"
+import StarRating from "../StarRating.tsx";
 
-const Star = ({value}: { value: number }) => {
-    const iconStyle = {fontSize: {xs: 15, sm: 15, md: 15, lg: 15}, color: "gold"};
-    if (value === 1) return <StarIcon sx={iconStyle}/>;
-    if (value === 0.5) return <StarHalfIcon sx={iconStyle}/>;
-    return <StarOutlineIcon sx={iconStyle}/>;
-};
+
+type CommentType = {
+    id: number;
+    nombre: string;
+    calificacion: number;
+    comentario: string;
+}
+
+
+const comments: CommentType[] = [
+    {id: 1, nombre: "Maria antonieta", calificacion: 5, comentario:"klsjadlaksdjadskj asdkljaskjdlkajsd lkjsadlkjasdkjas klasdjasdkljadslkjasdlkj"},
+    {id: 2, nombre: "Saki to wea", calificacion: 3, comentario:"Muy buena la comida"},
+    {id: 3, nombre: "Foca", calificacion: 3.3, comentario:"asdjkdaslkjsalk asdklajsdnmfaw kfajsdkjfasdlkjdfsan kldasjlkfsda sdakomasfdlkmasd kljdasfkmdsfalkmasd klasfjkljdflasfd kljfdslkmafsdklfsdaj kjdsflm,asdfilsdfa ,kljsdfamasdfm i"},
+];
 
 const LastCommentCard = () => {
 
-    const rating = 3.2;
-    const fullStars = Math.floor(rating);
-    const hasHalfStar = rating % 1 >= 0.5;
-    const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+    const objSize = {xs: 15, sm: 15, md: 15, lg: 15};
 
     return (
         <Box sx={{
-            backgroundColor: 'blue',
             width: "100%",
             height: "100%",
             display: "flex",
             flexDirection: 'column',
             alignItems: 'center',
-            gap: "12px",
+            gap: "16px",
             paddingBlock: "12px",
             paddingInline: "24px",
         }}>
-            <Paper sx={{
-                width: "100%",
-                padding: "8px",
-                display: "flex",
-                flexDirection: 'column',
-                gap: "8px",
-            }}>
-                {/*Seccion de los datos del usuario que hizo el comentario*/}
-                <Box sx={{
-                    display: "flex",
-                    flexDirection: 'row',
-                    gap: "8px"
-                }}>
-                    <Avatar>M</Avatar>
-                    <Box>
-                        <Typography>Maria antonieta</Typography>
-                        <Box sx={{display: 'flex', flexWrap: 'wrap', alignItems: 'center'}}>
-                            {[...Array(fullStars)].map((_, i) => (
-                                <Star key={`full-${i}`} value={1}/>
-                            ))}
-                            {hasHalfStar && <Star value={0.5}/>
-                            }
-                            {[...Array(emptyStars)].map((_, i) => (
-                                <Star key={`empty-${i}`} value={0}/>
-                            ))}
-                        </Box>
-                    </Box>
-
-                </Box>
-                {/*Seccion del comentario*/}
-                <Box sx={{width: '100%' }}>
-                    <Typography
+            {comments.map((comment: CommentType) => (
+                <Paper
+                    elevation={4}
+                    index={comment.id}
                     sx={{
-                        wordWrap: 'break-word', overflowWrap: 'break-word', whiteSpace: 'normal', fontSize: '0.8rem',
-                    }}
-                    >asdkljljljljlj ljljljljl jljljljljljljl jljljljljljljljljljljlj ljljljljljljljjaskdljas dsadjlsadjlkasjldasj</Typography>
+                        width: "100%",
+                        padding: "8px",
+                        display: "flex",
+                        flexDirection: 'column',
+                        gap: "8px",
+                    }}>
+                    {/*Seccion de los datos del usuario que hizo el comentario*/}
+                    <Box sx={{
+                        display: "flex",
+                        flexDirection: 'row',
+                        gap: "8px"
+                    }}>
+                        <Avatar>{(comment.nombre).charAt(0)}</Avatar>
+                        <Box>
+                            <Typography>{comment.nombre}</Typography>
+                            <StarRating cant={comment.calificacion} objSize={objSize} />
+                        </Box>
 
-                </Box>
-            </Paper>
+                    </Box>
+                    {/*Seccion del comentario*/}
+                    <Box sx={{width: '100%' }}>
+                        <Typography
+                            sx={{
+                                wordWrap: 'break-word', overflowWrap: 'break-word', whiteSpace: 'normal', fontSize: '0.8rem',
+                            }}
+                        >{comment.comentario}</Typography>
+
+                    </Box>
+                </Paper>
+            ))}
+
         </Box>
     )
 }
--- pymes-web/project-pyme-frontend/src/components/MainPage/StarCardBento.tsx
+++ pymes-web/project-pyme-frontend/src/components/MainPage/StarCardBento.tsx
@@ -4,23 +4,15 @@ import StarOutlineIcon from '@mui/icons-material/StarOutline';
 import {Box,Chip, Button} from '@mui/material';
 
 import {useLocation, useNavigate} from 'react-router-dom'
+import StarRating from "../StarRating.tsx";
 
-
-const Star = ({value}: { value: number }) => {
-    const iconStyle = {fontSize: {xs: 70, sm: 50, md: 60, lg: 80}, color: "gold"};
-    if (value === 1) return <StarIcon sx={iconStyle}/>;
-    if (value === 0.5) return <StarHalfIcon sx={iconStyle}/>;
-    return <StarOutlineIcon sx={iconStyle}/>;
-};
 
 const StarCardBento = () => {
 
     const navigate = useNavigate();
 
     const rating = 5;
-    const fullStars = Math.floor(rating);
-    const hasHalfStar = rating % 1 >= 0.5;
-    const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+    const objSize = {xs: 70, sm: 50, md: 60, lg: 80};
 
 
     return (
@@ -34,16 +26,7 @@ const StarCardBento = () => {
             gap: "20px"
         }}>
 
-            <Box sx={{display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center'}}>
-                {[...Array(fullStars)].map((_, i) => (
-                    <Star key={`full-${i}`} value={1}/>
-                ))}
-                {hasHalfStar && <Star value={0.5}/>
-                }
-                {[...Array(emptyStars)].map((_, i) => (
-                    <Star key={`empty-${i}`} value={0}/>
-                ))}
-            </Box>
+            <StarRating cant={rating} objSize={objSize}/>
             <Chip sx={{marginTop: "2px"}} label={"3.000 Reseñas"}/>
             <Button
                 sx={{marginTop: "10px"}}
